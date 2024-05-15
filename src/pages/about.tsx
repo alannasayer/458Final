fa039ed02803921d72ea76640720b16128147063
Alanna <115130607+alannasayer@users.noreply.github.com>
2024-05-14 22:51:11 -0700

    Update about.tsx

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Layout from '../components/layout';
 import sportsData from '../data/sportsData.json';
-import Calendar from 'react-calendar';
+import Calendar, { CalendarTileProperties } from 'react-calendar';
 import 'react-calendar/dist/Calendar.css';
 
 interface Event {
